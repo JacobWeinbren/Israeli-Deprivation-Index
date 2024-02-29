6eac5d751a63835561412b06cd4dcbc8bff75089
Jacob Weinbren <jacobweinbren@gmail.com>
2024-02-29 20:09:53 +0000

    Matched locations more broadly

--- merge.py
+++ merge.py
@@ -1,37 +1,49 @@
 import pandas as pd
 import json
+import re
 
-# Load the Excel file and rename columns directly
-df = pd.read_excel("deprivation.xlsx", header=9).rename(
-    columns={"CODE OF LOCALITY": "SEMEL_YISH", "CODE OF STATISTICAL AREA": "STAT08"}
+# Load the CSV file and rename columns directly
+df = pd.read_csv("Socio-Economical 2019.csv").rename(
+    columns={
+        "town_code": "SEMEL_YISH",
+        "STAT19": "STAT11",
+        "SocioRank19": "RANK_2019",
+        "town_hebrew": "SHEM_YISHU",
+    }
 )
-
-print(df.columns)
 
 # Load the GeoJSON file
 with open("statistical_areas.geojson", "r") as f:
     geojson_data = json.load(f)
 
+
+# Function to remove all spaces and non-Hebrew characters
+def clean_hebrew(input_string):
+    return re.sub(r"[^\u0590-\u05FF]", "", str(input_string))
+
+
+# Preprocess SHEM_YISHU in DataFrame for efficient matching
+df["SHEM_YISHU_CLEANED"] = df["SHEM_YISHU"].apply(clean_hebrew)
+
 # Initialize an empty list to hold features with matches
 matched_features = []
 
 for feature in geojson_data["features"]:
-    # Extract SEMEL_YISH and STAT08 from the feature
+    # Extract properties for matching
     semel_yish = feature["properties"]["SEMEL_YISH"]
-    stat08 = feature["properties"]["STAT08"]
+    stat11 = feature["properties"].get("STAT11")
+    shem_yishu = clean_hebrew(feature["properties"]["SHEM_YISHU"])
 
-    # Find the corresponding row in the DataFrame
-    deprivation_info = df.loc[
-        (df["SEMEL_YISH"] == semel_yish) & (df["STAT08"] == stat08),
-        ["RANK 2019[3] ", " CLUSTER 2019[4] "],
+    # Attempt to find the corresponding row in the DataFrame
+    match = df.loc[
+        ((df["SEMEL_YISH"] == semel_yish) & (df["STAT11"] == stat11))
+        | (df["SHEM_YISHU_CLEANED"] == shem_yishu),
+        ["RANK_2019"],
     ]
 
-    # If there's a match, update the GeoJSON feature properties and add to the matched_features list
-    if not deprivation_info.empty:
-        feature["properties"]["RANK_2019"] = deprivation_info.iloc[0]["RANK 2019[3] "]
-        feature["properties"]["CLUSTER_2019"] = deprivation_info.iloc[0][
-            " CLUSTER 2019[4] "
-        ]
+    # If there's a match, update the GeoJSON feature properties
+    if not match.empty:
+        feature["properties"]["RANK_2019"] = match.iloc[0]["RANK_2019"]
         matched_features.append(feature)
 
 # Replace the original features with the matched features
